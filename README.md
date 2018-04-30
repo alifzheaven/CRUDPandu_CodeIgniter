d845587cc5917280a7078563032fb4b118678567
alifzheaven <heavenalifz@gmail.com>
2018-04-30 15:18:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CRUDPandu_CodeIgniter
---
+++ application/views/pages/tentang_index.php
@@ -0,0 +1,7 @@
+<div class="card card-default" style="border-radius: 0px;margin: 20px 0;">
+     <div class="card-header">Tentang Pembuatan Website</div>
+     <div class="card-body">
+     	<center><img src="/ci_crud/assets/image/ptw.jpeg" width="200px" height="250px"></center>
+          <p>Assalammualaikum wr wb.<br/> Perkenalkan nama saya Pandu Teguh Wibawa kelas XI RPL 2 . <br/>Tujuan pembuatan website ini untuk belajar codeigniter dan memenuhi tugas Web Dinamis XI materi CRUD (Create, Read, Update & Delete). <br/> wassalammualaikum wr wb.</p>
+     </div>
+</div>
